64ab63e614151f89f933f29bfa82e83c4c35f99e
Berkay çelik <berkayc78@gmail.com>
2020-05-02 18:10:16 +0300

    initialize the spring boot application

---
+++ src/test/java/com/paydaybank/dashboard/ProductDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paydaybank.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
